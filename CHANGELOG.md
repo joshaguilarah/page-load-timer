b36f3ec15d359aa9eb7f221b44a77e6aa7a5b504
Josh Aguilar <jaguilar@allhomes.com.au>
2017-06-01 12:59:40 +1000

    Add simple validation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased][]
+### Added
+- Add simple validation
 
 ## [0.1.1][] - 2017-06-01
 ### Fixed
--- src/cli.js
+++ src/cli.js
@@ -5,4 +5,10 @@ import getPageLoadTimeAverage from './';
 
 const options = parseArgs(process.argv);
 
+if (!options || !options.url || !options.numRequests) {
+  console.error('Required parameters not specified.');
+  console.error('Usage: page-load-timer --url=<string> --numRequests=<integer>');
+  process.exit(1);
+}
+
 getPageLoadTimeAverage(options);
